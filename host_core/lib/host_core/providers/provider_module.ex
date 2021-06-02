a4cd5fbf50b3c348353b695be2c792a597033f75
Kevin Hoffman <alothien@gmail.com>
2021-06-02 15:33:21 -0400

    cleaning up provider termination.

--- host_core/lib/host_core/providers/provider_module.ex
+++ host_core/lib/host_core/providers/provider_module.ex
@@ -1,56 +1,74 @@
 defmodule HostCore.Providers.ProviderModule do
-    use GenServer, restart: :transient    
+    use GenServer, restart: :temporary
     require Logger
 
     defmodule State do
-        defstruct [:os_port, :os_pid]
+        defstruct [:os_port, :os_pid, :link_name, :contract_id, :public_key]
     end
 
     @doc """
     Starts the provider module assuming it is an executable file
     """
     def start_link(opts) do
-        GenServer.start_link(__MODULE__, opts)        
+        GenServer.start_link(__MODULE__, opts)
+    end
+
+    def identity_tuple(pid) do
+        GenServer.call(pid, :identity_tuple)
+    end
+
+    def cleanup(pid) do
+        GenServer.cast(pid, :cleanup)
     end
 
 
     @impl true
-    def init({:executable, path, public_key, link_name}) do
+    def init({:executable, path, public_key, link_name, contract_id}) do
         Logger.info("Starting executable capability provider at  '#{path}'")
         port = Port.open({:spawn, "#{path}"}, [:binary])
-        {:os_pid, pid} = Port.info(port, :os_pid)        
+        {:os_pid, pid} = Port.info(port, :os_pid)
 
-        publish_provider_started(public_key, link_name)
+        # Worth pointing out here that this process doesn't need to subscribe to
+        # the provider's NATS topic. The provider now subscribes to that directly
+        # when it starts.
+        publish_provider_started(public_key, link_name, contract_id)
 
-        {:ok, %State{ os_port: port, os_pid: pid}}
-    end  
+        {:ok, %State{ os_port: port,
+                       os_pid: pid,
+                   public_key: public_key,
+                    link_name: link_name,
+                  contract_id: contract_id }}
+    end
 
     @impl true
-    def terminate(reason, state) do
-        Logger.info("Terminating provider #{reason}")
+    def handle_call(:cleanup, _from, state) do
         if state.os_pid != nil do
             System.cmd("kill", ["-9", "#{state.os_pid}"])
         end
-
-        nil
+        {:noreply, state}
     end
-    
+
+    @impl true
+    def handle_call(:identity_tuple, _from, state) do
+        {:reply, {state.public_key, state.link_name}, state}
+    end
+
+    @impl true
+    def handle_call(:get_pk, _from, state) do
+        {:reply, state.public_key, state}
+    end
+
+    @impl true
+    def handle_call(:get_link_name, _from, state) do
+        {:reply, state.link_name, state}
+    end
+
     @impl true
     def handle_info({_ref, {:data, logline}}, state) do
         Logger.info("Provider: #{logline}")
 
         {:noreply, state}
     end
-    
-    def handle_info({:msg,
-        %{
-            body: body,            
-            reply_to: reply_to,            
-            topic: topic,            
-        }}, state) do
-        Logger.info("Received invocation on #{topic}")
-        {:noreply, state}
-    end    
 
     def handle_info({_ref, msg}, state) do
         Logger.info(msg)
@@ -58,17 +76,35 @@ defmodule HostCore.Providers.ProviderModule do
         {:noreply, state}
     end
 
-    defp lattice_subscribe(public_key, link_name) do
-        prefix = HostCore.Host.lattice_prefix()
-        {:ok, _subscription} = Gnat.sub(:lattice_nats, self(), 
-            "wasmbus.rpc.#{prefix}.#{public_key}.#{link_name}")
-    end
-
-    defp publish_provider_started(pk, link_name) do
+    def publish_provider_stopped(public_key, link_name) do
         prefix = HostCore.Host.lattice_prefix()
         stamp = DateTime.utc_now() |> DateTime.to_iso8601
 
-        host = HostCore.Host.host_key()        
+        host = HostCore.Host.host_key()
+        msg = %{
+            specversion: "1.0",
+            time: stamp,
+            type: "com.wasmcloud.lattice.provider_stopped",
+            source: "#{host}",
+            datacontenttype: "application/json",
+            id: UUID.uuid4(),
+            data: %{
+                public_key: public_key,
+                link_name: link_name
+            }
+        }
+        |> Cloudevents.from_map!()
+        |> Cloudevents.to_json()
+        topic = "wasmbus.ctl.#{prefix}.events"
+
+        Gnat.pub(:control_nats, topic, msg)
+    end
+
+    defp publish_provider_started(pk, link_name, contract_id) do
+        prefix = HostCore.Host.lattice_prefix()
+        stamp = DateTime.utc_now() |> DateTime.to_iso8601
+
+        host = HostCore.Host.host_key()
         msg = %{
             specversion: "1.0",
             time: stamp,
@@ -78,9 +114,10 @@ defmodule HostCore.Providers.ProviderModule do
             id: UUID.uuid4(),
             data: %{
                 public_key: pk,
-                link_name: link_name
+                link_name: link_name,
+                contract_id: contract_id
             }
-        } 
+        }
         |> Cloudevents.from_map!()
         |> Cloudevents.to_json()
         topic = "wasmbus.ctl.#{prefix}.events"
--- host_core/lib/host_core/providers/provider_supervisor.ex
+++ host_core/lib/host_core/providers/provider_supervisor.ex
@@ -1,19 +1,52 @@
 defmodule HostCore.Providers.ProviderSupervisor do
     use DynamicSupervisor
+    require Logger
+    alias HostCore.Providers.ProviderModule
 
     def start_link(init_arg) do
       DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
     end
-  
+
     @impl true
-    def init(_init_arg) do    
+    def init(_init_arg) do
 
       DynamicSupervisor.init(strategy: :one_for_one)
 
     end
 
-    def start_executable_provider(path, public_key, link_name) do               
-        DynamicSupervisor.start_child(__MODULE__, {HostCore.Providers.ProviderModule, {:executable, path, public_key, link_name}})
-    end    
-    
+    def start_executable_provider(path, public_key, link_name, contract_id) do
+        DynamicSupervisor.start_child(__MODULE__, {ProviderModule, {:executable, path, public_key, link_name, contract_id}})
+    end
+
+    def handle_info({:EXIT, _pid, reason}, state) do
+      Logger.info("A child process died: #{reason}")
+      {:noreply, state}
+    end
+
+    def handle_info(msg, state) do
+      Logger.error("Supervisor received unexpected message: #{inspect(msg)}")
+      {:noreply, state}
+    end
+
+    def terminate_provider(public_key, link_name) do
+
+      provs = providers_by_pid()
+      |> Enum.filter(fn { {pk, ln}, _pid} -> pk == public_key && ln == link_name end)
+      |> Enum.take(1)
+
+      if length(provs) == 1 do
+        [{{pk, ln}, pid}] = provs
+        ProviderModule.cleanup(pid)
+        ProviderModule.publish_provider_stopped(pk, ln)
+        Process.exit(pid, :manual_shutdown)
+      end
+
+    end
+
+    defp providers_by_pid() do
+      Supervisor.which_children(__MODULE__)
+      |> Enum.map(fn {_id, pid, _type_, _modules} ->
+        { ProviderModule.identity_tuple(pid), pid} end)
+    end
+
 end
