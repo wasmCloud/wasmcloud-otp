2f1e8c18bfecc5406149cabb40715f1ddba1a6c7
Brooks Townsend <brooksmtownsend@gmail.com>
2021-06-10 10:00:48 -0400

    figured out how to import from coreui js

--- wasmcloud_host/assets/vendor/wasmcloud/js/popovers.js
+++ wasmcloud_host/assets/vendor/wasmcloud/js/popovers.js
@@ -1,4 +1,4 @@
-import { Popover } from '@coreui/coreui'
+import { Popover } from '../../@coreui/coreui-pro/js/coreui.bundle.min'
 document.querySelectorAll('[data-toggle="popover"]').forEach(function (element) {
   // eslint-disable-next-line no-new
   new Popover(element);
