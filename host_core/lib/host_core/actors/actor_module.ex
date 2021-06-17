be12faf085c17d73aaec96a66ee5eed9be8f24c8
Kevin Hoffman <alothien@gmail.com>
2021-06-17 09:26:20 -0400

    Changing actor subscription for RPC to a queue subscription

--- host_core/lib/host_core/actors/actor_module.ex
+++ host_core/lib/host_core/actors/actor_module.ex
@@ -111,9 +111,10 @@ defmodule HostCore.Actors.ActorModule do
     end
 
     prefix = HostCore.Host.lattice_prefix()
+    topic = "wasmbus.rpc.#{prefix}.#{claims.public_key}"
 
     {:ok, _subscription} =
-      Gnat.sub(:lattice_nats, self(), "wasmbus.rpc.#{prefix}.#{claims.public_key}")
+      Gnat.sub(:lattice_nats, self(), topic, queue_group: topic)
 
     imports = %{
       wapc: Imports.wapc_imports(agent),
