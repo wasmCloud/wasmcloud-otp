3c2064668eecc2712f8d3b3b791a7608da223214
Kevin Hoffman <alothien@gmail.com>
2021-06-25 08:22:13 -0400

    Bump to 0.20.0, bump wasmex and rustler versions

--- host_core/mix.exs
+++ host_core/mix.exs
@@ -4,7 +4,7 @@ defmodule HostCore.MixProject do
   def project do
     [
       app: :host_core,
-      version: "0.1.0",
+      version: "0.20.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       compilers: [:rustler] ++ Mix.compilers(),
@@ -29,9 +29,9 @@ defmodule HostCore.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:wasmex, "~> 0.3.1"},
+      {:wasmex, "~> 0.4.0"},
       {:msgpax, "~> 2.3"},
-      {:rustler, "~> 0.21.1"},
+      {:rustler, "~> 0.22.0"},
       {:gnat, "~> 1.2"},
       {:cloudevents, "~> 0.4.0"},
       {:uuid, "~> 1.1"},
