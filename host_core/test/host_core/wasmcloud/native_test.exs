b44b16bc557a8f5d6e9c02b8f590f97180332cf7
Brooks Townsend <brooksmtownsend@gmail.com>
2021-07-01 10:08:36 -0400

    updated makefile with par steps
    updated binary file sizes

--- host_core/test/host_core/wasmcloud/native_test.exs
+++ host_core/test/host_core/wasmcloud/native_test.exs
@@ -23,10 +23,10 @@ defmodule HostCore.WasmCloud.NativeTest do
     target_bytes =
       case :os.type() do
         {:unix, :darwin} ->
-          7_814_928
+					7_823_168
 
         {:unix, _linux} ->
-          7_911_908
+					7_912_797
 
         {:win32, :nt} ->
           7_865_344
