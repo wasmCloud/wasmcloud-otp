8c56ac436b6c1eb0fee8e3ee8e2c22d64ad68e88
Brooks Townsend <brooksmtownsend@gmail.com>
2021-07-08 09:08:29 -0400

    update action to release test image to azurecr
    move comment that's apparently messing with updating locales
    
    it was curl the whole time
    
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- wasmcloud_host/rel/scripts/apt-build-install.sh
+++ wasmcloud_host/rel/scripts/apt-build-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 apt update && \
   DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
   curl \
@@ -11,4 +13,9 @@ apt update && \
   curl -fsSL https://deb.nodesource.com/setup_14.x | bash - && \
   apt install -y --no-install-recommends nodejs && \
   # Install yarn package manager via npm
-  npm install --global yarn
+  npm install --global yarn && \
+  # Set locales for elixir
+  export LANG=en_US.UTF-8 && \
+    echo $LANG UTF-8 > /etc/locale.gen && \
+    locale-gen && \
+    update-locale LANG=$LANG
--- wasmcloud_host/rel/scripts/apt-release-install.sh
+++ wasmcloud_host/rel/scripts/apt-release-install.sh
@@ -1,8 +1,10 @@
+#!/bin/bash -x
+
 apt update && \
   DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
   build-essential \
-  # Set proper locale encoding for Elixir (required)
-  locale && \
+  curl \
+  locales && \
   export LANG=en_US.UTF-8 && \
     echo $LANG UTF-8 > /etc/locale.gen && \
     locale-gen && \
