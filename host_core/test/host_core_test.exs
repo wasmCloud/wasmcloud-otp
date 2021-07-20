ddc38f4aefc059bea09ab1dd0f73a37218c81f90
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2021-07-20 10:51:26 -0400

    Adding a purge function to HostCore.Host that empties actors and providers gracefully
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- host_core/test/host_core_test.exs
+++ host_core/test/host_core_test.exs
@@ -2,6 +2,12 @@ defmodule HostCoreTest do
   use ExUnit.Case
   doctest HostCore
 
+  @httpserver_path "priv/providers/httpserver"
+  @echo_path "priv/actors/echo_s.wasm"
+  @httpserver_key "VAG3QITQQ2ODAOWB5TTQSDJ53XK3SHBEIFNK4AYJ5RKAX2UNSCAPHA5M"
+  @httpserver_link "default"
+  @httpserver_contract "wasmcloud:httpserver"
+
   test "greets the world" do
     assert HostCore.hello() == :world
   end
@@ -22,5 +28,30 @@ defmodule HostCoreTest do
     assert family_target == labels["hostcore.osfamily"]
     # HOST_ prefix removed.
     assert "42" == labels["testing"]
+  end
+
+  test "Host purges actors and providers" do
+    {:ok, bytes} = File.read("priv/actors/echo_s.wasm")
+    {:ok, _pid} = HostCore.Actors.ActorSupervisor.start_actor(bytes)
+
+    {:ok, _pid} =
+      HostCore.Providers.ProviderSupervisor.start_executable_provider(
+        @httpserver_path,
+        @httpserver_key,
+        @httpserver_link,
+        @httpserver_contract
+      )
+
+    Process.sleep(1_000)
+    HostCore.Host.purge()
+    Process.sleep(2_000)
+
+    actor_count =
+      HostCore.Actors.ActorSupervisor.all_actors()
+      |> Map.keys()
+      |> length
+
+    assert actor_count == 0
+    assert HostCore.Providers.ProviderSupervisor.all_providers() == []
   end
 end
