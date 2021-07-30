87aa0acb3d924d648c3261df294e51b9ed078c36
Brooks Townsend <brooksmtownsend@gmail.com>
2021-07-30 11:14:58 -0400

    implemented error handling for link definitions
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- wasmcloud_host/lib/wasmcloud_host_web/live/components/define_link_component.ex
+++ wasmcloud_host/lib/wasmcloud_host_web/live/components/define_link_component.ex
@@ -2,7 +2,7 @@ defmodule DefineLinkComponent do
   use Phoenix.LiveComponent
 
   def mount(socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:error_msg, nil)}
   end
 
   def handle_event("validate", _params, socket) do
@@ -12,32 +12,61 @@ defmodule DefineLinkComponent do
 
   def handle_event(
         "define_link",
-        %{
-          "actor_id" => actor_id,
-          "contract_id" => contract_id,
-          "link_name" => link_name,
-          "provider_id" => provider_id,
-          "values" => values
-        },
+        linkdef,
         socket
       ) do
+    IO.inspect(linkdef)
+    actor_id = Map.get(linkdef, "actor_id")
+    provider_id = Map.get(linkdef, "provider_id")
+    contract_id = Map.get(linkdef, "contract_id")
+    link_name = Map.get(linkdef, "link_name")
+    values = Map.get(linkdef, "values")
+
+    IO.inspect(values)
+
     values_map =
-      values
-      |> String.split(",")
-      |> Enum.flat_map(fn s -> String.split(s, "=") end)
-      |> Enum.chunk_every(2)
-      |> Enum.map(fn [a, b] -> {a, b} end)
-      |> Map.new()
+      case values do
+        "" ->
+          Map.new()
 
-    HostCore.Linkdefs.Manager.put_link_definition(
-      actor_id,
-      contract_id,
-      link_name,
-      provider_id,
-      values_map
-    )
+        nil ->
+          Map.new()
 
-    {:noreply, socket}
+        value_list ->
+          value_list
+          |> String.split(",")
+          |> Enum.flat_map(fn s -> String.split(s, "=") end)
+          |> Enum.chunk_every(2)
+          |> Enum.map(fn [a, b] -> {a, b} end)
+          |> Map.new()
+      end
+
+    error_msg =
+      cond do
+        actor_id == nil ->
+          "Please select an Actor ID to link"
+
+        provider_id == nil ->
+          "Please select a Provider ID to link"
+
+        true ->
+          case HostCore.Linkdefs.Manager.put_link_definition(
+                 actor_id,
+                 contract_id,
+                 link_name,
+                 provider_id,
+                 values_map
+               ) do
+            :ok -> nil
+            _any -> "Error publishing link definition"
+          end
+      end
+
+    if error_msg == nil do
+      Phoenix.PubSub.broadcast(WasmcloudHost.PubSub, "frontend", :hide_modal)
+    end
+
+    {:noreply, assign(socket, error_msg: error_msg)}
   end
 
   def render(assigns) do
@@ -83,14 +112,14 @@ defmodule DefineLinkComponent do
       <div class="form-group row">
         <label class="col-md-3 col-form-label" for="linkdef-linkname-input">Link Name</label>
         <div class="col-md-9">
-          <input class="form-control" id="linkdef-linkname-input" type="text" name="link_name" placeholder="default" value="default">
+          <input class="form-control" id="linkdef-linkname-input" type="text" name="link_name" placeholder="default" value="default" readonly>
           <span class="help-block">Select a provider to autopopulate the link name</span>
         </div>
       </div>
       <div class="form-group row">
         <label class="col-md-3 col-form-label" for="linkdef-contractid-input">Contract ID</label>
         <div class="col-md-9">
-          <input class="form-control" id="linkdef-contractid-input" type="text" name="contract_id" placeholder="wasmcloud:contract">
+          <input class="form-control" id="linkdef-contractid-input" type="text" name="contract_id" placeholder="wasmcloud:contract" readonly>
           <span class="help-block">Select a provider to autopopulate the contract id</span>
         </div>
       </div>
@@ -103,9 +132,14 @@ defmodule DefineLinkComponent do
       </div>
       <div class="modal-footer">
         <button class="btn btn-secondary" type="button" phx-click="hide_modal">Close</button>
-        <button class="btn btn-primary" type="submit" phx-click="hide_modal">Submit</button>
+        <button class="btn btn-primary" type="submit">Submit</button>
       </div>
     </form>
+    <%= if @error_msg != nil do %>
+    <div class="alert alert-danger">
+    <%= @error_msg %>
+    </div>
+    <% end %>
     """
   end
 end
