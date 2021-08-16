9db3d0c1c8ceba6d411760a2659b4122d9c52066
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2021-08-16 09:31:49 -0400

    Closing #174 - fixing bugs in numbergen (#175)
    * Closing #174 - fixing bugs in numbergen
    
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>
    
    * convert msgpack of single value to bitstring
    
    Signed-off-by: steve <dev@somecool.net>
    
    * Removing the -1, ensuring that random in range and random32 both operate the same way.
    
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>
    
    Co-authored-by: steve <dev@somecool.net>

--- host_core/lib/host_core/providers/builtin/numbergen.ex
+++ host_core/lib/host_core/providers/builtin/numbergen.ex
@@ -1,17 +1,17 @@
 defmodule HostCore.Providers.Builtin.Numbergen do
   def invoke("GenerateGuid", _payload) do
-    Msgpax.pack!(UUID.uuid4())
+    IO.iodata_to_binary(Msgpax.pack!(UUID.uuid4()))
   end
 
   def invoke("RandomInRange", payload) do
     params = Msgpax.unpack!(payload)
 
-    min = min(params["min"], 0)
-    max = max(params["max"], 4_294_967_295)
-    Msgpax.pack!(Enum.random(min..max))
+    min = max(params["min"], 0)
+    max = min(params["max"], 4_294_967_295)
+    IO.iodata_to_binary(Msgpax.pack!(Enum.random(min..max)))
   end
 
   def invoke("Random32", _payload) do
-    Msgpax.pack!(floor(:rand.uniform(4_294_967_294)))
+    IO.iodata_to_binary(Msgpax.pack!(Enum.random(0..4_294_967_295)))
   end
 end
