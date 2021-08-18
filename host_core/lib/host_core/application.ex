cee25472f70ff1f68e627480351ff468a057b9b3
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2021-08-18 18:23:51 -0400

    Adding support for .env hierarchy of files to supply environment configuration (#189)
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- host_core/lib/host_core/application.ex
+++ host_core/lib/host_core/application.ex
@@ -3,62 +3,10 @@ defmodule HostCore.Application do
   # for more information on OTP Applications
   @moduledoc false
   require Logger
-
   use Application
 
-  @prefix_var "WASMCLOUD_LATTICE_PREFIX"
-  @hostkey_var "WASMCLOUD_HOST_KEY"
-  @hostseed_var "WASMCLOUD_HOST_SEED"
-  @default_prefix "default"
-
-  def config!() do
-    {host_key, host_seed} = HostCore.WasmCloud.Native.generate_key(:server)
-    {def_cluster_key, def_cluster_seed} = HostCore.WasmCloud.Native.generate_key(:cluster)
-
-    s =
-      Hashids.new(
-        salt: "lc_deliver_inbox",
-        min_len: 2
-      )
-
-    hid = Hashids.encode(s, Enum.random(1..4_294_967_295))
-
-    providers = [
-      %Vapor.Provider.Env{
-        bindings: [
-          {:cache_deliver_inbox, "_DI", default: "_INBOX.#{hid}"},
-          {:host_key, @hostkey_var, default: host_key},
-          {:host_seed, @hostseed_var, default: host_seed},
-          {:lattice_prefix, @prefix_var, default: @default_prefix},
-          {:rpc_host, "WASMCLOUD_RPC_HOST", default: "0.0.0.0"},
-          {:rpc_port, "WASMCLOUD_RPC_PORT", default: 4222, map: &String.to_integer/1},
-          {:rpc_seed, "WASMCLOUD_RPC_SEED", default: ""},
-          {:rpc_timeout, "WASMCLOUD_RPC_TIMEOUT_MS", default: 2000, map: &String.to_integer/1},
-          {:rpc_jwt, "WASMCLOUD_RPC_JWT", default: ""},
-          {:prov_rpc_host, "WASMCLOUD_PROV_RPC_HOST", default: "0.0.0.0"},
-          {:prov_rpc_port, "WASMCLOUD_PROV_RPC_PORT", default: 4222, map: &String.to_integer/1},
-          {:prov_rpc_seed, "WASMCLOUD_PROV_RPC_SEED", default: ""},
-          {:prov_rpc_jwt, "WASMCLOUD_PROV_RPC_JWT", default: ""},
-          {:ctl_host, "WASMCLOUD_CTL_HOST", default: "0.0.0.0"},
-          {:ctl_port, "WASMCLOUD_CTL_PORT", default: 4222, map: &String.to_integer/1},
-          {:ctl_seed, "WASMCLOUD_CTL_SEED", default: ""},
-          {:ctl_jwt, "WASMCLOUD_CTL_JWT", default: ""},
-          {:cluster_seed, "WASMCLOUD_CLUSTER_SEED", default: def_cluster_seed},
-          {:cluster_issuers, "WASMCLOUD_CLUSTER_ISSUERS", default: def_cluster_key},
-          {:provider_delay, "WASMCLOUD_PROV_SHUTDOWN_DELAY_MS",
-           default: 300, map: &String.to_integer/1},
-          {:allow_latest, "WASMCLOUD_OCI_ALLOW_LATEST", default: false, map: &String.to_atom/1},
-          {:allowed_insecure, "WASMCLOUD_OCI_ALLOWED_INSECURE",
-           default: [], map: &String.split(&1, ",")}
-        ]
-      }
-    ]
-
-    Vapor.load!(providers)
-  end
-
   def start(_type, _args) do
-    config = config!()
+    config = Vapor.load!(HostCore.ConfigPlan)
 
     children = [
       {Registry, keys: :unique, name: Registry.ProviderRegistry},
---
+++ host_core/lib/host_core/config_plan.ex
@@ -0,0 +1,56 @@
+defmodule HostCore.ConfigPlan do
+  @behaviour Vapor.Plan
+
+  alias Vapor.Provider.{Dotenv, Env}
+
+  @prefix_var "WASMCLOUD_LATTICE_PREFIX"
+  @hostkey_var "WASMCLOUD_HOST_KEY"
+  @hostseed_var "WASMCLOUD_HOST_SEED"
+  @default_prefix "default"
+
+  @impl Vapor.Plan
+  def config_plan do
+    {host_key, host_seed} = HostCore.WasmCloud.Native.generate_key(:server)
+    {def_cluster_key, def_cluster_seed} = HostCore.WasmCloud.Native.generate_key(:cluster)
+
+    s =
+      Hashids.new(
+        salt: "lc_deliver_inbox",
+        min_len: 2
+      )
+
+    hid = Hashids.encode(s, Enum.random(1..4_294_967_295))
+
+    [
+      %Dotenv{},
+      %Env{
+        bindings: [
+          {:cache_deliver_inbox, "_DI", default: "_INBOX.#{hid}"},
+          {:host_key, @hostkey_var, default: host_key},
+          {:host_seed, @hostseed_var, default: host_seed},
+          {:lattice_prefix, @prefix_var, default: @default_prefix},
+          {:rpc_host, "WASMCLOUD_RPC_HOST", default: "0.0.0.0"},
+          {:rpc_port, "WASMCLOUD_RPC_PORT", default: 4222, map: &String.to_integer/1},
+          {:rpc_seed, "WASMCLOUD_RPC_SEED", default: ""},
+          {:rpc_timeout, "WASMCLOUD_RPC_TIMEOUT_MS", default: 2000, map: &String.to_integer/1},
+          {:rpc_jwt, "WASMCLOUD_RPC_JWT", default: ""},
+          {:prov_rpc_host, "WASMCLOUD_PROV_RPC_HOST", default: "0.0.0.0"},
+          {:prov_rpc_port, "WASMCLOUD_PROV_RPC_PORT", default: 4222, map: &String.to_integer/1},
+          {:prov_rpc_seed, "WASMCLOUD_PROV_RPC_SEED", default: ""},
+          {:prov_rpc_jwt, "WASMCLOUD_PROV_RPC_JWT", default: ""},
+          {:ctl_host, "WASMCLOUD_CTL_HOST", default: "0.0.0.0"},
+          {:ctl_port, "WASMCLOUD_CTL_PORT", default: 4222, map: &String.to_integer/1},
+          {:ctl_seed, "WASMCLOUD_CTL_SEED", default: ""},
+          {:ctl_jwt, "WASMCLOUD_CTL_JWT", default: ""},
+          {:cluster_seed, "WASMCLOUD_CLUSTER_SEED", default: def_cluster_seed},
+          {:cluster_issuers, "WASMCLOUD_CLUSTER_ISSUERS", default: def_cluster_key},
+          {:provider_delay, "WASMCLOUD_PROV_SHUTDOWN_DELAY_MS",
+           default: 300, map: &String.to_integer/1},
+          {:allow_latest, "WASMCLOUD_OCI_ALLOW_LATEST", default: false, map: &String.to_atom/1},
+          {:allowed_insecure, "WASMCLOUD_OCI_ALLOWED_INSECURE",
+           default: [], map: &String.split(&1, ",")}
+        ]
+      }
+    ]
+  end
+end
