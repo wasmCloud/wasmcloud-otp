b89872839674dc697f70fde415c3df2b9fcba4b6
stevelr <steve@cosmonic.com>
2021-08-31 15:50:29 -0700

    accept new-style method prefixes and old-style method-only names.
    Signed-off-by: stevelr <steve@cosmonic.com>

--- host_core/lib/host_core/providers/builtin/numbergen.ex
+++ host_core/lib/host_core/providers/builtin/numbergen.ex
@@ -1,9 +1,9 @@
 defmodule HostCore.Providers.Builtin.Numbergen do
-  def invoke("GenerateGuid", _payload) do
+  def invoke(method, _payload) when method=="NumberGen.GenerateGuid" or method=="GenerateGuid" do
     IO.iodata_to_binary(Msgpax.pack!(UUID.uuid4()))
   end
 
-  def invoke("RandomInRange", payload) do
+  def invoke(method, payload) when method=="NumberGen.RandomInRange" or method=="RandomInRange" do
     params = Msgpax.unpack!(payload)
 
     min = max(params["min"], 0)
@@ -11,7 +11,7 @@ defmodule HostCore.Providers.Builtin.Numbergen do
     IO.iodata_to_binary(Msgpax.pack!(Enum.random(min..max)))
   end
 
-  def invoke("Random32", _payload) do
+  def invoke(method, _payload) when method=="NumberGen.Random32" or method=="RandomRandom32" do
     IO.iodata_to_binary(Msgpax.pack!(Enum.random(0..4_294_967_295)))
   end
 end
