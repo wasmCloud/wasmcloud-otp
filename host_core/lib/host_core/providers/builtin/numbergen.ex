1f4601b0104f2e9458fc59befbd8ce12564f7aa0
stevelr <steve@cosmonic.com>
2021-08-31 16:26:14 -0700

    ran mix-format
    Signed-off-by: stevelr <steve@cosmonic.com>

--- host_core/lib/host_core/providers/builtin/numbergen.ex
+++ host_core/lib/host_core/providers/builtin/numbergen.ex
@@ -1,9 +1,11 @@
 defmodule HostCore.Providers.Builtin.Numbergen do
-  def invoke(method, _payload) when method=="NumberGen.GenerateGuid" or method=="GenerateGuid" do
+  def invoke(method, _payload)
+      when method == "NumberGen.GenerateGuid" or method == "GenerateGuid" do
     IO.iodata_to_binary(Msgpax.pack!(UUID.uuid4()))
   end
 
-  def invoke(method, payload) when method=="NumberGen.RandomInRange" or method=="RandomInRange" do
+  def invoke(method, payload)
+      when method == "NumberGen.RandomInRange" or method == "RandomInRange" do
     params = Msgpax.unpack!(payload)
 
     min = max(params["min"], 0)
@@ -11,7 +13,8 @@ defmodule HostCore.Providers.Builtin.Numbergen do
     IO.iodata_to_binary(Msgpax.pack!(Enum.random(min..max)))
   end
 
-  def invoke(method, _payload) when method=="NumberGen.Random32" or method=="RandomRandom32" do
+  def invoke(method, _payload)
+      when method == "NumberGen.Random32" or method == "RandomRandom32" do
     IO.iodata_to_binary(Msgpax.pack!(Enum.random(0..4_294_967_295)))
   end
 end
