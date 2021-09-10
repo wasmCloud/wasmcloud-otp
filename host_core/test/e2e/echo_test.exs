aef10f2f07257e65c527be6030006aaed2b85ff9
Brooks Townsend <brooksmtownsend@gmail.com>
2021-09-10 10:50:20 -0400

    Make tests deterministic (#199)
    * add event watcher module for deterministic tests
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * update makefile to support jetstream
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    initial implementation of generic assert_received?
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    refactored event_watcher to be more generic
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    use evnt watcher in kvcounter, remove process.sleep
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    replace remaining actor/provider sleeps, add docs
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    add wait_for_linkdef, remove all Process.sleep calls
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    implement waiting for count of event
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * purge event stream on watcher init
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    added debug messages
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    add rudimentary jetstream test for debugging
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    debug steps
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    uncomment purge, wait for first ping
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    remove PR build_tarballs_manual
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * remove copious IO.inspect calls
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- host_core/test/e2e/echo_test.exs
+++ host_core/test/e2e/echo_test.exs
@@ -1,15 +1,25 @@
 defmodule HostCore.E2E.EchoTest do
   use ExUnit.Case, async: false
 
+  setup do
+    {:ok, evt_watcher} =
+      GenServer.start_link(HostCoreTest.EventWatcher, HostCore.Host.lattice_prefix())
+
+    [
+      evt_watcher: evt_watcher
+    ]
+  end
+
   @echo_key "MADQAFWOOOCZFDKYEYHC7AUQKDJTP32XUC5TDSMN4JLTDTU2WXBVPG4G"
   @httpserver_link "default"
   @httpserver_path "test/fixtures/providers/httpserver.par.gz"
 
-  test "echo roundtrip" do
+  test "echo roundtrip", %{:evt_watcher => evt_watcher} do
     {:ok, bytes} = File.read("test/fixtures/actors/echo_s.wasm")
     {:ok, _pid} = HostCore.Actors.ActorSupervisor.start_actor(bytes)
-
     on_exit(fn -> HostCore.Actors.ActorSupervisor.terminate_actor(@echo_key, 1) end)
+
+    :ok = HostCoreTest.EventWatcher.wait_for_actor_start(evt_watcher, @echo_key)
 
     {:ok, _pid} =
       HostCore.Providers.ProviderSupervisor.start_provider_from_file(
@@ -22,11 +32,17 @@ defmodule HostCore.E2E.EchoTest do
     httpserver_key = par.claims.public_key
     httpserver_contract = par.contract_id
 
-    Process.sleep(1000)
-
     on_exit(fn ->
       HostCore.Providers.ProviderSupervisor.terminate_provider(httpserver_key, @httpserver_link)
     end)
+
+    :ok =
+      HostCoreTest.EventWatcher.wait_for_provider_start(
+        evt_watcher,
+        httpserver_contract,
+        @httpserver_link,
+        httpserver_key
+      )
 
     actor_count =
       Map.get(HostCore.Actors.ActorSupervisor.all_actors(), @echo_key)
@@ -37,8 +53,6 @@ defmodule HostCore.E2E.EchoTest do
     assert elem(Enum.at(HostCore.Providers.ProviderSupervisor.all_providers(), 0), 0) ==
              httpserver_key
 
-    Process.sleep(1000)
-
     :ok =
       HostCore.Linkdefs.Manager.put_link_definition(
         @echo_key,
@@ -48,11 +62,15 @@ defmodule HostCore.E2E.EchoTest do
         %{PORT: "8080"}
       )
 
-    Process.sleep(1000)
+    :ok =
+      HostCoreTest.EventWatcher.wait_for_linkdef(
+        evt_watcher,
+        @echo_key,
+        httpserver_contract,
+        @httpserver_link
+      )
 
     HTTPoison.start()
     {:ok, _resp} = HTTPoison.get("http://localhost:8080/foo/bar")
-
-    Process.sleep(1000)
   end
 end
