178e18ae2c46debac4b58663cc5ef684ed65d438
Brooks Townsend <brooks@cosmonic.com>
2021-09-14 11:47:11 -0400

    remove nil registry lookups from all_providers
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- host_core/lib/host_core/providers/provider_supervisor.ex
+++ host_core/lib/host_core/providers/provider_supervisor.ex
@@ -133,6 +133,7 @@ defmodule HostCore.Providers.ProviderSupervisor do
     |> Enum.map(fn {_d, pid, _type, _modules} ->
       provider_for_pid(pid)
     end)
+    |> Enum.filter(fn provider -> provider != nil end)
   end
 
   def provider_for_pid(pid) do
