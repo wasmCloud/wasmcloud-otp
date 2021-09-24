321c7a6c61d4622cb2a719db30f5f299530a4ff5
Taylor Thomas <taylor.thomas@microsoft.com>
2021-09-24 12:38:34 -0600

    Adds windows tarballs to pipeline (#227)
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

--- wasmcloud_host/config/prod.exs
+++ wasmcloud_host/config/prod.exs
@@ -1,18 +1,5 @@
 use Mix.Config
 
-# For production, NIFs should instead be loaded from predetermined directories
-# This is for cross-compilation compatibility
-#
-# For ease of use, use `make build-prod` to automatically build the NIFs for production
-config :host_core, HostCore.WasmCloud.Native,
-  crate: :hostcore_wasmcloud_native,
-  load_from: {:host_core, "priv/native/libhostcore_wasmcloud_native"},
-  skip_compilation?: true
-
-config :wasmex, Wasmex.Native,
-  load_from: {:wasmex, "priv/native/libwasmex"},
-  skip_compilation?: true
-
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
