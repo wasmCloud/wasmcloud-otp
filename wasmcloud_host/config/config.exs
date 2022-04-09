145f17e5f1c2d221bc368d94a6ae6f5931a6c309
Steve <steve@cosmonic.com>
2022-04-09 13:27:00 -0700

    replace deprecated Mix.Config with Config
    Signed-off-by: Steve <steve@cosmonic.com>

--- wasmcloud_host/config/config.exs
+++ wasmcloud_host/config/config.exs
@@ -1,11 +1,11 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the .Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 # Configures the endpoint
 config :wasmcloud_host, WasmcloudHostWeb.Endpoint,
@@ -56,4 +56,4 @@ config :dart_sass,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
