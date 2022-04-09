35f9237270652a9a5ef83ab1a827ec475403e0cb
Steve <steve@cosmonic.com>
2022-04-09 16:03:30 -0700

    update credo, add erlavro dep to fix error message about deprecated rebar2
    Signed-off-by: Steve <steve@cosmonic.com>

--- host_core/mix.exs
+++ host_core/mix.exs
@@ -35,6 +35,8 @@ defmodule HostCore.MixProject do
       {:rustler, "~> 0.24.0"},
       {:jason, "~> 1.2.2"},
       {:gnat, "~> 1.2"},
+      # erlavro isn't used, but this version upgrades dependency of cloudevents 0.4.0 to use rebar3
+      {:erlavro, "~> 2.9.7", override: true, manager: :rebar3},
       {:cloudevents, "~> 0.4.0"},
       {:uuid, "~> 1.1"},
 
@@ -44,7 +46,7 @@ defmodule HostCore.MixProject do
       {:hashids, "~> 2.0"},
       {:parallel_task, "~> 0.1.1"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:httpoison, "~> 1.8", only: [:test]},
       {:json, "~> 1.4", only: [:test]},
       {:distillery, "~> 2.0"}
