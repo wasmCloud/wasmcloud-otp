8c68a17a5677f03eaf055872e9e59d3eefaf54b8
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2022-04-12 15:06:00 -0400

    Terminate gracefully during SIGTERM
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- host_core/mix.exs
+++ host_core/mix.exs
@@ -1,7 +1,7 @@
 defmodule HostCore.MixProject do
   use Mix.Project
 
-  @app_vsn "0.54.1"
+  @app_vsn "0.54.2"
 
   def project do
     [
--- wasmcloud_host/mix.exs
+++ wasmcloud_host/mix.exs
@@ -1,7 +1,7 @@
 defmodule WasmcloudHost.MixProject do
   use Mix.Project
 
-  @app_vsn "0.54.1"
+  @app_vsn "0.54.2"
 
   def project do
     [
