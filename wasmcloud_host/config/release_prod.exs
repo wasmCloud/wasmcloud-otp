921153fef5bcabef0007609a21a12f5273ab348b
Brooks Townsend <brooks@cosmonic.com>
2022-06-02 17:21:49 -0400

    removed recursive config import
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- wasmcloud_host/config/release_prod.exs
+++ wasmcloud_host/config/release_prod.exs
@@ -2,7 +2,6 @@
 # is for specifying custom parameters for the Native host_core NIF
 import Config
 
-import_config "prod.secret.exs"
 import_config "prod.exs"
 
 config :host_core, HostCore.WasmCloud.Native,
