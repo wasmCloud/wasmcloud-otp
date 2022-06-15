6c5d8d539b33b5e54c5b70d6da40d7ccfc05f9fd
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2022-06-15 16:01:54 -0400

    FOURMATgit statusgit status
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- wasmcloud_host/config/config.exs
+++ wasmcloud_host/config/config.exs
@@ -24,7 +24,6 @@ config :wasmcloud_host, WasmcloudHostWeb.Endpoint,
 config :host_core,
   retain_rpc_subscriptions: false
 
-
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
