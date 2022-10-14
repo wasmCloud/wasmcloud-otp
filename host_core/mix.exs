7a4e5a9f56ff3749daa9ab74162150c91103ac00
Brooks Townsend <brooks@cosmonic.com>
2022-10-14 09:19:44 -0400

    commented out host_core release action
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    amazing fix ahead: try bumping
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    nif build
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    try checking out some extra debug things
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    commented out host_core release
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- host_core/mix.exs
+++ host_core/mix.exs
@@ -41,9 +41,9 @@ defmodule HostCore.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:wasmex, "~> 0.7.0"},
+      {:wasmex, "~> 0.7.1"},
       {:msgpax, "~> 2.3"},
-      {:rustler, "~> 0.24.0"},
+      {:rustler, "~> 0.25.0"},
       {:timex, "~> 3.7"},
       {:jason, "~> 1.2.2"},
       {:gnat, "~> 1.5.2"},
