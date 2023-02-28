66995e0598c967034d6dbf5ad62e2099d84dc037
Brooks Townsend <brooks@cosmonic.com>
2023-02-28 10:35:43 -0500

    fixed benchmark common path
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- host_core/test/test_helper.exs
+++ host_core/test/test_helper.exs
@@ -1,4 +1,4 @@
-Code.require_file("benchmark/common.exs", __DIR__)
+Code.require_file("host_core/benchmark/common.exs", __DIR__)
 Code.require_file("support/event_watcher.exs", __DIR__)
 Code.require_file("support/common.exs", __DIR__)
 Code.require_file("support/constants.exs", __DIR__)
