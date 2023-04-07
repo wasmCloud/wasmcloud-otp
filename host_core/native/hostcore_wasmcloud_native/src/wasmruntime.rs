5de23e9124805b77b5b89dd1a50f71ffc040a561
Roman Volosatovs <rvolosatovs@riseup.net>
2023-04-07 16:13:29 +0200

    feat: update to WIT-enabled wasmCloud
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- host_core/native/hostcore_wasmcloud_native/src/wasmruntime.rs
+++ host_core/native/hostcore_wasmcloud_native/src/wasmruntime.rs
@@ -1,35 +1,45 @@
+use std::ops::Deref;
+use std::sync::{Arc, Condvar, Mutex};
+
+use crate::{
+    atoms,
+    environment::{CallbackToken, CallbackTokenResource},
+};
+
 use anyhow::{self, bail, Context};
 use async_trait::async_trait;
 use log::{error, trace};
-use rand::{thread_rng, Rng, RngCore};
-
-use crate::environment::CallbackToken;
 use rustler::{
     env::{OwnedEnv, SavedTerm},
     resource::ResourceArc,
     types::tuple::make_tuple,
     Binary, Encoder, Env, Error, LocalPid, NifResult, Term,
 };
-
-use std::sync::{Condvar, Mutex};
 use wascap::jwt;
-use wasmcloud::{
-    capability, logging, numbergen, Actor, Handle, HostInvocation, LoggingInvocation,
-    NumbergenInvocation, Runtime as WcRuntime,
-};
-
-use crate::{atoms, environment::CallbackTokenResource};
+use wasmcloud_host::{host, logging, Actor, Runtime as WcRuntime};
 
 /// A wrapper around an instance of the wasmCloud runtime. This will be used inside a `ResourceArc` to allow
 /// Elixir to maintain a long-lived reference to it
 pub struct RuntimeResource {
-    pub inner: WcRuntime,
+    inner: WcRuntime,
+    /// Runtime process ID
+    pid: LocalPid,
 }
 
 /// A wrapper around an instance of a precompiled wasmCloud actor. This will be used inside a `ResourceArc` to allow
 /// Elixir to maintain a long-lived reference to it
+#[derive(Clone)]
 pub struct ActorResource {
-    pub actor: Actor,
+    inner: Actor,
+    runtime_pid: LocalPid,
+}
+
+impl Deref for ActorResource {
+    type Target = Actor;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
 }
 
 #[derive(NifStruct)]
@@ -38,114 +48,110 @@ pub struct ExRuntimeConfig {
     host_id: String,
 }
 
+#[derive(Clone)]
 pub struct ElixirHandler {
-    // Runtime pid
+    /// Runtime process ID
     pid: LocalPid,
-    #[allow(unused)]
-    host_id: String,
+    claims: jwt::Claims<jwt::Actor>,
+    call_context: Vec<u8>,
+}
+
+#[derive(Clone)]
+struct ElixirHandlerArc(Arc<ElixirHandler>);
+
+impl Deref for ElixirHandlerArc {
+    type Target = ElixirHandler;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<ElixirHandler> for ElixirHandlerArc {
+    fn from(handler: ElixirHandler) -> Self {
+        Self(handler.into())
+    }
 }
 
 #[async_trait]
-impl Handle<capability::Invocation> for ElixirHandler {
-    async fn handle(
-        &self,
-        claims: &jwt::Claims<jwt::Actor>,
+impl logging::Host for ElixirHandlerArc {
+    async fn log(
+        &mut self,
+        level: logging::Level,
+        _context: String,
+        message: String,
+    ) -> anyhow::Result<()> {
+        // note: current OTP host does not have a trace log level
+        let level = match level {
+            logging::Level::Trace => {
+                trace!("drop `trace` level log");
+                return Ok(());
+            }
+            logging::Level::Debug => "debug",
+            logging::Level::Info => "info",
+            logging::Level::Warn => "warn",
+            logging::Level::Error => "error",
+            logging::Level::Critical => "error", // TODO: Implement this level in OTP
+        };
+        let callback_token = new_callback_token();
+
+        let mut msg_env = OwnedEnv::new();
+        msg_env.send_and_clear(&self.pid, |env| {
+            (
+                atoms::perform_actor_log(),
+                crate::Claims::from(self.claims.clone()),
+                level,
+                message,
+                &callback_token,
+            )
+                .encode(env)
+        });
+
+        let mut result = callback_token.token.return_value.lock().unwrap();
+        while result.is_none() {
+            result = callback_token.token.continue_signal.wait(result).unwrap();
+        }
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl host::Host for ElixirHandlerArc {
+    async fn call(
+        &mut self,
         binding: String,
-        invocation: capability::Invocation,
-        call_context: &Option<Vec<u8>>,
-    ) -> anyhow::Result<Option<Vec<u8>>> {
-        match invocation {
-            capability::Invocation::Logging(LoggingInvocation::WriteLog { level, text }) => {
-                // note: current OTP host does not have a trace log level
-                let level = match level {
-                    logging::Level::Debug => "debug",
-                    logging::Level::Info => "info",
-                    logging::Level::Warn => "warn",
-                    logging::Level::Error => "error",
-                };
-                let callback_token = new_callback_token();
+        namespace: String,
+        operation: String,
+        payload: Option<Vec<u8>>,
+    ) -> anyhow::Result<Result<Option<Vec<u8>>, String>> {
+        let mut msg_env = OwnedEnv::new();
+        let callback_token = new_callback_token();
+        msg_env.send_and_clear(&self.pid, |env| {
+            (
+                atoms::invoke_callback(),
+                crate::Claims::from(self.claims.clone()),
+                (binding, namespace, operation),
+                &payload,
+                &self.call_context,
+                &callback_token,
+            )
+                .encode(env)
+        });
 
-                let mut msg_env = OwnedEnv::new();
-                msg_env.send_and_clear(&self.pid.clone(), |env| {
-                    (
-                        atoms::perform_actor_log(),
-                        crate::Claims::from(claims.clone()),
-                        level,
-                        text,
-                        callback_token.clone(),
-                    )
-                        .encode(env)
-                });
-
-                let mut result = callback_token.token.return_value.lock().unwrap();
-                while result.is_none() {
-                    result = callback_token.token.continue_signal.wait(result).unwrap();
-                }
-
-                // we don't actually care about the result from the host here
-
-                Ok(None)
-            }
-
-            capability::Invocation::Numbergen(NumbergenInvocation::GenerateGuid) => {
-                let mut buf = uuid::Bytes::default();
-                thread_rng().fill_bytes(&mut buf);
-                let guid = uuid::Builder::from_random_bytes(buf)
-                    .into_uuid()
-                    .to_string();
-                trace!("generated GUID: `{guid}`");
-                numbergen::serialize_response(&guid).map(Some)
-            }
-
-            capability::Invocation::Numbergen(NumbergenInvocation::RandomInRange { min, max }) => {
-                let v = thread_rng().gen_range(min..=max);
-                trace!("generated random u32 in range [{min};{max}]: {v}");
-                numbergen::serialize_response(&v).map(Some)
-            }
-
-            capability::Invocation::Numbergen(NumbergenInvocation::Random32) => {
-                let v: u32 = thread_rng().gen();
-                trace!("generated random u32: {v}");
-                numbergen::serialize_response(&v).map(Some)
-            }
-
-            capability::Invocation::Host(HostInvocation {
-                namespace,
-                operation,
-                payload,
-            }) => {
-                let mut msg_env = OwnedEnv::new();
-                let callback_token = new_callback_token();
-                msg_env.send_and_clear(&self.pid.clone(), |env| {
-                    (
-                        atoms::invoke_callback(),
-                        crate::Claims::from(claims.clone()),
-                        (binding, namespace, operation),
-                        payload.unwrap_or_default(),
-                        call_context.clone().unwrap_or_default(),
-                        callback_token.clone(),
-                    )
-                        .encode(env)
-                });
-
-                let mut result = callback_token.token.return_value.lock().unwrap();
-                while result.is_none() {
-                    result = callback_token.token.continue_signal.wait(result).unwrap();
-                }
-                match result
-                    .as_ref()
-                    .expect("expect callback token to contain a result")
-                {
-                    (true, payload) => Ok(Some(payload.clone())),
-                    (false, e) => {
-                        // TODO: verify whether we should return none here or use an Err
-                        error!("Elixir callback threw an exception.");
-                        let err_str = String::from_utf8(e.to_vec()).unwrap_or_else(|parse_err| {
-                            format!("failed to parse inv err: {parse_err}")
-                        });
-                        bail!("Host call function failed: {err_str}")
-                    }
-                }
+        let mut result = callback_token.token.return_value.lock().unwrap();
+        while result.is_none() {
+            result = callback_token.token.continue_signal.wait(result).unwrap();
+        }
+        match result
+            .as_ref()
+            .expect("expect callback token to contain a result")
+        {
+            (true, payload) => Ok(Ok(Some(payload.clone()))),
+            (false, e) => {
+                // TODO: verify whether we should return none here or use an Err
+                error!("Elixir callback threw an exception.");
+                bail!("Host call function failed: {e:?}")
             }
         }
     }
@@ -171,18 +177,15 @@ pub fn on_load(env: Env) -> bool {
 #[rustler::nif(name = "runtime_new")]
 pub fn new(
     env: rustler::Env<'_>,
-    ExRuntimeConfig { host_id }: ExRuntimeConfig,
+    _config: ExRuntimeConfig,
 ) -> Result<ResourceArc<RuntimeResource>, rustler::Error> {
-    let handler: Box<dyn Handle<capability::Invocation>> = Box::new(ElixirHandler {
-        pid: env.pid(),
-        host_id,
-    });
-    let rt = WcRuntime::new(handler)
+    let inner = WcRuntime::new()
         .context("failed to construct runtime")
         .map_err(|e| Error::Term(Box::new(e.to_string())))?;
-
-    let resource = ResourceArc::new(RuntimeResource { inner: rt });
-    Ok(resource)
+    Ok(ResourceArc::new(RuntimeResource {
+        inner,
+        pid: env.pid(),
+    }))
 }
 
 #[rustler::nif(name = "version")]
@@ -197,16 +200,16 @@ pub fn version(runtime_resource: ResourceArc<RuntimeResource>) -> Result<String,
 #[allow(unused_variables)]
 pub fn start_actor<'a>(
     env: rustler::Env<'a>,
-    runtime_resource: ResourceArc<RuntimeResource>,
+    runtime: ResourceArc<RuntimeResource>,
     bytes: Binary<'a>,
 ) -> Result<ResourceArc<ActorResource>, rustler::Error> {
-    let actor = Actor::new(&runtime_resource.inner, bytes.as_slice())
+    let inner = Actor::new(&runtime.inner, bytes.as_slice())
         .context("failed to load actor from bytes")
         .unwrap();
-
-    let ar = ActorResource { actor };
-
-    Ok(ResourceArc::new(ar))
+    Ok(ResourceArc::new(ActorResource {
+        inner,
+        runtime_pid: runtime.pid,
+    }))
 }
 
 // This does not need to be on a dirty scheduler as it simply spawns a TOKIO
@@ -214,7 +217,7 @@ pub fn start_actor<'a>(
 #[rustler::nif(name = "call_actor")]
 pub fn call_actor<'a>(
     env: rustler::Env<'a>,
-    component: ResourceArc<ActorResource>,
+    actor: ResourceArc<ActorResource>,
     operation: &str,
     payload: Binary<'a>,
     call_context: Binary<'a>,
@@ -233,11 +236,16 @@ pub fn call_actor<'a>(
     // and use other sync mechanisms to finish the work and send
     // the results to the caller (the `from` field)
 
+    let runtime_pid = actor.runtime_pid;
+    let (actor, claims) = actor.inner.clone().into_configure_claims();
+    let handler = ElixirHandlerArc::from(ElixirHandler {
+        pid: runtime_pid,
+        claims,
+        call_context,
+    });
+    let actor = actor.logging(handler.clone()).host(handler);
     crate::spawn(async move {
-        let response = component
-            .actor
-            .call_with_context(operation, Some(payload), call_context)
-            .await;
+        let response = actor.call(operation, Some(payload)).await;
         thread_env.send_and_clear(&pid, |thread_env| {
             send_actor_call_response(thread_env, from, response)
         });
