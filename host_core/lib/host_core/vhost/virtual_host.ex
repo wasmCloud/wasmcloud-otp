a5d0107591c42ab2c502243412b5db90d3466ee7
Connor Smith <connor.smith.256@gmail.com>
2023-04-14 15:21:31 -0600

    BREAKING: pass log_level in HostData (#610)
    * pass log_level in HostData
    
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>
    
    * fix typo
    
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>
    
    * breaking: replace structured_log_level with log_level
    
    The new config value applies to structured and unstructured logging.
    To maintain backwards-compatibility, the old environment variable is
    mapped to the new field
    
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>
    
    * warn on use of deprecated var
    
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>
    
    * fix test
    
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>
    
    ---------
    
    Signed-off-by: Connor Smith <connor.smith.256@gmail.com>

--- host_core/lib/host_core/vhost/virtual_host.ex
+++ host_core/lib/host_core/vhost/virtual_host.ex
@@ -329,7 +329,8 @@ defmodule HostCore.Vhost.VirtualHost do
       invocation_seed: config.cluster_seed,
       js_domain: config.js_domain,
       # In case providers want to be aware of this for their own logging
-      structured_logging: config.enable_structured_logging
+      structured_logging: config.enable_structured_logging,
+      log_level: config.log_level
     })
   end
 
